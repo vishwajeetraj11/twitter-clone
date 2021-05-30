ee3fe5754e6bdadfa0705d231dfebbc6c3baeef4
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-05-30 22:06:18 +0530

    Added server-side validation

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -8,8 +8,21 @@ router.get("/", (req,res, next) => {
     res.status(200).render("register")
 })
 router.post("/", (req,res, next) => {
-    console.log(req.body)
-    res.status(200).render("register")
+    const firstName = req.body.firstName.trim();
+    const lastName = req.body.lastName.trim();
+    const username = req.body.username.trim();
+    const email = req.body.email.trim();
+    const password = req.body.password;
+
+    const payload = req.body;
+
+    if(firstName && lastName && username && email && password) {
+
+    }
+    else {
+        payload.errorMessage = "Make sure each field has a valid value.";
+        res.status(200).render("register", payload);
+    }
 })
 
 export default router;
