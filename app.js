0e2ede34c9febb761b563b4e1928e1a69d47eea1
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-05-30 15:21:47 +0530

    Added Middleware: requireLogin

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import {requireLogin} from "./middlewares.js"
 const app = express();
 
 const port = 3000;
@@ -14,7 +14,7 @@ app.set("view engine", "pug");
 // When the app needs a view go look at views folder
 app.set("views", "views")
 
-app.get("/", (req,res, next) => {
+app.get("/", requireLogin, (req,res, next) => {
 
     // Payload is just a term used to refer to a data that we are sending to a function or to a page or through request or something like that.
     const payload = {
---
+++ middlewares.js
@@ -0,0 +1,8 @@
+export const requireLogin = (req, res, next) => {
+    if(req.session && req.session.user) {
+        return next();
+    }
+    else {
+        return res.redirect('/login');
+    }
+}
