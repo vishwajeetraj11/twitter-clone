790469c07a7ce8101f0c1284706bd43a7287d335
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-05-30 22:21:10 +0530

    Adding User Schema

---
+++ models/UserModel.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+	firstName: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	profilePic: {
+		type: String,
+		default: '/images/profilePic.png',
+	},
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
