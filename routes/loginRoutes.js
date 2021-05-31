c3d6826ac375a015aecd527e29dc7072e31e40b1
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-05-31 22:00:35 +0530

    Login

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,14 +1,46 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/UserModel.js';
 
 const router = express.Router();
 
-app.set("view engine", "pug");
-app.set("views", "views")
+router.get('/', (req, res, next) => {
+	res.status(200).render('login');
+});
 
-router.get("/", (req,res, next) => {
+router.post('/', async (req, res, next) => {
+	const payload = req.body;
 
-    res.status(200).render("login")
-})
+	if (req.body.logUsername && req.body.logPassword) {
+		const user = await User.findOne({
+			$or: [
+				{ username: req.body.logUsername },
+				{ email: req.body.logUsername },
+			],
+		}).catch((error) => {
+			console.log(error);
+			payload.errorMessage = 'Something went wrong.';
+			res.status(200).render('login', payload);
+		});
+
+		if (user !== null) {
+			const result = await bcrypt.compare(
+				req.body.logPassword,
+				user.password
+			);
+
+			if (result === true) {
+				req.session.user = user;
+				return res.redirect('/');
+			}
+		}
+
+		payload.errorMessage = 'Login credentials incorrect.';
+		return res.status(200).render('login', payload);
+	}
+
+	payload.errorMessage = 'Make sure each field has a valid value.';
+	res.status(200).render('login');
+});
 
 export default router;
