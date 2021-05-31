416b5722a698d28f6be2e5746b994d9b8e4013b8
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-06-01 01:55:51 +0530

    tweet styling

--- public/js/common.js
+++ public/js/common.js
@@ -15,14 +15,58 @@ $('#postTextarea').keyup((event) => {
 });
 
 $('#submitPostButton').click((event) => {
-	var button = $(event.target);
-	var textbox = $('#postTextarea');
+	const button = $(event.target);
+	const textbox = $('#postTextarea');
 
-	var data = {
+	const data = {
 		content: textbox.val(),
 	};
 
 	$.post('/api/tweets', data, (postData, status, xhr) => {
-		console.log(postData);
+		const html = createPostHtml(postData);
+		$('.postsContainer').prepend(html);
+		textbox.val('');
+		button.prop('disabled', true);
 	});
 });
+
+function createPostHtml(postData) {
+	const postedBy = postData.postedBy;
+	const displayName = postedBy.firstName + ' ' + postedBy.lastName;
+	const timestamp = postData.createdAt;
+
+	return `<div class='post'>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}'>
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+							<div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-retweet'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-heart'></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
