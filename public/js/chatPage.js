01b134f7ab1428c63dd49b2ca0039516ba56e479
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-06-06 16:48:27 +0530

    Chat Page get message text from input

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -20,3 +20,33 @@ $('#chatNameButton').click(() => {
 		},
 	});
 });
+
+// Evebt Handler when send button is clicked.
+$('.sendMessageButton').click(() => {
+	messageSubmitted();
+});
+
+$('.inputTextbox').keydown((event) => {
+	if (event.which === 13 && !event.shiftKey) {
+		messageSubmitted();
+		return false;
+		// return false does'nt allow a new line in text area on enter  press
+	}
+});
+
+function messageSubmitted() {
+	console.log('submiited');
+}
+
+function messageSubmitted() {
+	const content = $('.inputTextbox').val().trim();
+
+	if (content != '') {
+		sendMessage(content);
+		$('.inputTextbox').val('');
+	}
+}
+
+function sendMessage(content) {
+	console.log(content);
+}
