27fc7ff7bdde36efa65a0d5e668b7f34fe15a020
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-06-12 20:20:24 +0530

    Added Port Fix

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express();
 // this works in case of pug ->
 app.use(express.urlencoded({ extended: false }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
 	console.log(`Server listening on port: ${port}`);
